4e26919ab03f14c1c0af711bd5d02682cf8704ce
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-04-29 09:46:52 +0800

    feat: add route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,23 @@
 import { lazy } from 'solid-js';
-import { Routes, Route } from '@solidjs/router';
+import { Routes, Route, Navigate, useNavigate, useLocation } from '@solidjs/router';
 
 const Home = lazy(() => import('../views/HomePage'));
+const About = lazy(() => import('../views/AboutPage'));
 
 export default () => (
     <Routes>
+        {/* Redirect to home */}
         <Route
             path="/"
+            element={<Navigate href="/home" />}
+        />
+        <Route
+            path="/home"
             component={Home}
+        />
+        <Route
+            path="/about"
+            component={About}
         />
     </Routes>
 );
---
+++ src/views/AboutPage/index.tsx
@@ -0,0 +1,6 @@
+export default () => (
+    <div class="about-page">
+        <h2 class="about-page-title">About</h2>
+        <p class="about-page-content">This page is an about page</p>
+    </div>
+);
