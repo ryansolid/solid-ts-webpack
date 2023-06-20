eb67312ac55a5940bc69a7dc54b77de9a7a3237f
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-06-21 00:57:13 +0800

    feat: add route guard

---
+++ src/routes/RouterGuard.tsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet } from '@solidjs/router';
+import { JSX } from 'solid-js';
+
+interface GuardedRouteProps {
+    /**
+     * Permission check for route
+     * @default false
+     */
+    isRouteAccessible?: boolean;
+    /**
+     * Route to be redirected to
+     * @default '/'
+     */
+    redirectRoute?: string;
+}
+
+export default function (props: GuardedRouteProps): JSX.Element {
+    const { isRouteAccessible, redirectRoute = '' } = props;
+    return isRouteAccessible ? <Outlet /> : <Navigate href={redirectRoute} />;
+}
