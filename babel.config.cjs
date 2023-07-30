0cc969c92570caee656e2bcaa1b1a6728bcde630
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-07-30 12:31:04 +0800

    feat: babel add corejs config

--- babel.config.cjs
+++ babel.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
             '@babel/preset-env',
             {
                 useBuiltIns: 'usage',
-                corejs: '3',
+                corejs: '3.31',
             },
         ],
         'solid',
